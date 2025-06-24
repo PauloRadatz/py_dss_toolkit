ed3da61575f0f0198faff35849c5710c4385f570
Paulo Radatz <paulo.radatz@gmail.com>
2025-06-24 10:40:47 -0400

    Only PD Elements in the loading and current violations

--- src/py_dss_toolkit/results/SnapShot/CurrentsLoading.py
+++ src/py_dss_toolkit/results/SnapShot/CurrentsLoading.py
@@ -12,11 +12,15 @@ class CurrentsLoading:
         imags_df, _ = create_currents_elements_dataframes(self._dss)
         loading_df = imags_df.copy()
         limit_type = get_violation_current_limit_type()
+        pd_element = list()
         for element in imags_df.index:
             self._dss.circuit.set_active_element(element)
-            amps = getattr(self._dss.cktelement, limit_type)
-            if amps > 0:
-                loading_df.loc[element] = (imags_df.loc[element] / amps) * 100
-            else:
-                loading_df.loc[element] = np.nan
-        return loading_df
+            if len(self._dss.cktelement.bus_names) > 1:  # It is a PD Element
+                pd_element.append(element)
+                amps = getattr(self._dss.cktelement, limit_type)
+                if amps > 0:
+                    loading_df.loc[element] = (imags_df.loc[element] / amps) * 100
+                else:
+                    loading_df.loc[element] = np.nan
+        return loading_df.loc[pd_element]
+
