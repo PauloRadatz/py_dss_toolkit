6c82b9ba766301414e4da75625867654f4e384f7
Paulo Radatz <paulo.radatz@gmail.com>
2025-10-16 06:40:27 -0400

    Adding nodes and enabled in SegmentsDF

--- src/py_dss_toolkit/model/SegmentsDF.py
+++ src/py_dss_toolkit/model/SegmentsDF.py
@@ -25,17 +25,30 @@ class SegmentsDF:
 
         elem_name_list = list()
         elem_bus1_list = list()
+        elem_nodes1_list = list()
         elem_bus2_list = list()
+        elem_nodes2_list = list()
         elem_type_list = list()
         elem_x1_list = list()
         elem_y1_list = list()
         elem_x2_list = list()
         elem_y2_list = list()
+        enabled_list = list()
         for elem in filtered_elements:
             self._dss.circuit.set_active_element(elem)
             elem_name_list.append(self._dss.cktelement.name.lower())
             elem_bus1_list.append(self._dss.cktelement.bus_names[0].split(".")[0])
+            nodes1 = self._dss.cktelement.bus_names[0].split(".")[1:]
+            if len(nodes1) == 0:
+                elem_nodes1_list.append(["1", "2", "3"])
+            else:
+                elem_nodes1_list.append(nodes1)
             elem_bus2_list.append(self._dss.cktelement.bus_names[1].split(".")[0])
+            nodes2 = self._dss.cktelement.bus_names[1].split(".")[1:]
+            if len(nodes2) == 0:
+                elem_nodes2_list.append(["1", "2", "3"])
+            else:
+                elem_nodes2_list.append(nodes2)
             elem_type_list.append(self._dss.cktelement.name.lower().split(".")[0])
 
             self._dss.circuit.set_active_bus(elem_bus1_list[-1])
@@ -46,14 +59,22 @@ class SegmentsDF:
             elem_x2_list.append(self._dss.bus.x)
             elem_y2_list.append(self._dss.bus.y)
 
+            if self._dss.text(f"? {elem}.enabled").lower() == "false":
+                enabled_list.append(False)
+            else:
+                enabled_list.append(True)
+
         dict_df = dict()
         dict_df["name"] = elem_name_list
         dict_df["bus1"] = elem_bus1_list
+        dict_df["nodes1"] = elem_nodes1_list
         dict_df["bus2"] = elem_bus2_list
+        dict_df["nodes2"] = elem_nodes2_list
         dict_df["type"] = elem_type_list
         dict_df["x1"] = elem_x1_list
         dict_df["y1"] = elem_y1_list
         dict_df["x2"] = elem_x2_list
         dict_df["y2"] = elem_y2_list
+        dict_df["enabled"] = enabled_list
 
         return pd.DataFrame.from_dict(dict_df)
