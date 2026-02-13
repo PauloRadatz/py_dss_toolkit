151fbdb76958751b806dd01fab0ae4277034cca5
Paulo Radatz <paulo.radatz@gmail.com>
2026-02-13 06:08:39 -0500

    New plot and organization in Snapshot

---
+++ src/py_dss_toolkit/view/interactive_view/SnapShot/Circuit/DistanceSettings.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author  : Paulo Radatz
+# @Email   : paulo.radatz@gmail.com
+
+from dataclasses import dataclass, field
+from py_dss_toolkit.view.interactive_view.SnapShot.Circuit.BaseSettingsNumerical import BaseSettingsNumerical
+
+@dataclass(kw_only=True)
+class DistanceSettings(BaseSettingsNumerical):
+    colorbar_title: str = field(init=True, repr=True, default="Distance from Meter (km)")
