39f64d81f9d3dcc275d4f3e4668ee99ebb242fe4
Paulo Radatz <paulo.radatz@gmail.com>
2026-02-24 06:43:29 -0500

    Running each test individually for Linux

--- tests/py_dss_toolkit/model/test_model_edit_element.py
+++ tests/py_dss_toolkit/model/test_model_edit_element.py
@@ -2,7 +2,7 @@ import pytest
 
 
 def test_edit_element_valid_properties(snapshot_study_13bus):
-    snapshot_study_13bus.model.add_element("load", "my_load", dict(phases=3, bus1=1, kv=4.16, kw=100))
+    snapshot_study_13bus.model.add_element("load", "my_load", dict(phases=3, bus1="632.1.2.3", kv=4.16, kw=100))
     snapshot_study_13bus.model.edit_element("load", "my_load", {"kw": "200", "kvar": "50"})
 
     df = snapshot_study_13bus.model.element_data("load", "my_load")
@@ -16,14 +16,14 @@ def test_edit_element_nonexistent_raises_value_error(snapshot_study_13bus):
 
 
 def test_edit_element_invalid_property_raises_value_error(snapshot_study_13bus):
-    snapshot_study_13bus.model.add_element("load", "my_load", dict(phases=3, bus1=1, kv=4.16, kw=100))
+    snapshot_study_13bus.model.add_element("load", "my_load", dict(phases=3, bus1="632.1.2.3", kv=4.16, kw=100))
 
     with pytest.raises(ValueError, match="does not have property"):
         snapshot_study_13bus.model.edit_element("load", "my_load", {"not_a_real_prop": "999"})
 
 
 def test_edit_element_case_insensitive_property(snapshot_study_13bus):
-    snapshot_study_13bus.model.add_element("load", "my_load", dict(phases=3, bus1=1, kv=4.16, kw=100))
+    snapshot_study_13bus.model.add_element("load", "my_load", dict(phases=3, bus1="632.1.2.3", kv=4.16, kw=100))
     snapshot_study_13bus.model.edit_element("load", "my_load", {"KW": "300"})
 
     df = snapshot_study_13bus.model.element_data("load", "my_load")
