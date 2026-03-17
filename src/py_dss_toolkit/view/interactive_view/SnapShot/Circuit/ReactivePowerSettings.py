862d1a7916702faab5aacae770c48ff00ac13e26
Paulo Radatz <paulo.radatz@gmail.com>
2026-03-17 13:22:05 -0400

    Adding file

---
+++ src/py_dss_toolkit/view/interactive_view/SnapShot/Circuit/ReactivePowerSettings.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author  : Paulo Radatz
+# @Email   : paulo.radatz@gmail.com
+# @File    : ReactivePowerSettings.py
+# @Software: PyCharm
+
+from dataclasses import dataclass, field
+from py_dss_toolkit.view.interactive_view.SnapShot.Circuit.BaseSettingsNumerical import BaseSettingsNumerical
+
+
+@dataclass(kw_only=True)
+class ReactivePowerSettings(BaseSettingsNumerical):
+    colorbar_title: str = field(init=True, repr=True, default="Reactive Power (kvar)")
